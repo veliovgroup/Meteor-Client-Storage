019fb1aa8c49191665d0c17c5e1f5a83a55fbc18
Dmitriy A. Golev <dr.dimitru@gmail.com>
2015-07-23 18:53:27 +0300

    v1.0.1
     - Update `ostrio:cookies` package dependency

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:cstorage',
-  version: '1.0.0',
+  version: '1.0.1',
   summary: 'Boilerplate Client storage functions, localStorage with fall-back to Cookies',
   git: 'https://github.com/VeliovGroup/Meteor-Client-Storage',
   documentation: 'README.md'
@@ -8,12 +8,12 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.use(['coffeescript', 'localstorage', 'ostrio:cookies@1.0.0'], 'client');
+  api.use(['coffeescript', 'localstorage', 'ostrio:cookies@1.0.1'], 'client');
   api.addFiles('cstorage.coffee', 'client');
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use(['coffeescript', 'ostrio:jsextensions@0.0.4', 'ostrio:cstorage@1.0.0'], 'client');
+  api.use(['coffeescript', 'ostrio:jsextensions@0.0.4', 'ostrio:cstorage@1.0.1'], 'client');
   api.addFiles('cstorage-tests.js', 'client');
 });
